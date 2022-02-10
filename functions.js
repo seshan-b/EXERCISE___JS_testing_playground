ae185f4d433dd3fe06507416a8b50ff553f148f5
seshan-b <seshan.b@gmail.com>
2022-02-10 22:17:41 +1300

    Made changes

--- functions.js
+++ functions.js
@@ -2,101 +2,77 @@
 
 // Create function
 function foo() {
-    console.log('This is foo')
+  console.log('This is foo')
 }
 
 // Invoke
 foo()
 
-
-
 // Passing arguments
 function passingArgs(x, y, z) {
-
-
-    console.log('Passing arguments', x, y, z)
+  console.log('Passing arguments', x, y, z)
 }
 
 // Invoke and pass values
 passingArgs(1, 2, 3)
 
-
-
 // Passing Variable in arguments
 
 function passingVariablesInFunction(a, b, c) {
+  // a = 1
+  // b = 2
+  // c = 3
 
-    // a = 1
-    // b = 2
-    // c = 3
+  // Invoking function and passing in dummy variables
+  passingArgs(a, b, c)
 
-    // Invoking function and passing in dummy variables
-    passingArgs(a, b, c)
-
-
-    console.log('Passing variables', a, b, c)
+  console.log('Passing variables', a, b, c)
 }
-
 
 // Initializing an object
-function initializeObj () {
+function initializeObj() {
+  let a = {
+    name: 'Allen',
+  }
 
- let a = {
-     name: 'Allen'
- }
+  let b = 10
+  let c = 20
 
-let b = 10
-let c = 20
+  console.log('Passing objects')
 
-console.log('Passing objects')
-
-
-return passingVariablesInFunction(a, b, c)
-
+  return passingVariablesInFunction(a, b, c)
 }
-
-
-
 
 // Invoke function
 initializeObj()
 
 function aFunction() {
-    console.log('Calling A function')
+  console.log('Calling A function')
 }
 
 // Calling an function from another
 
 function callingAnotherFunctionInFunction(func) {
+  func = aFunction()
 
-    func = aFunction()
+  console.log('Function: ', func)
 
-
-    console.log('Function: ', func)
-
-    return func
+  return func
 }
 
-
 // Invoke function
-passingVariablesInFunction(a = 800, b = 100, c = 200)
-
-
+passingVariablesInFunction((a = 800), (b = 100), (c = 200))
 
 // Arrow function
 const anArrowFunction = (a, b, c) => {
-    console.log('This is an arrow function' )
-    console.log('Log a: ',  a)
-    return a
+  console.log('This is an arrow function')
+  console.log('Log a: ', a)
+  return a
 }
 
-function setTimeout(foo, time)  {
-
-    console.log('Return foo');
-    foo()
+function setTimeout(foo, time) {
+  console.log('Return foo')
+  foo()
 }
-
 
 setTimeout(foo, 10000)
-
-
--- reduce.js
+++ reduce.js
@@ -1,187 +1,151 @@
 const people = [
-        { id: "1", name: "Leigh", age: 35 },
-        { id: "2", name: "Jenny", age: 30 },
-        { id: "3", name: "Heather", age: 28 },
-    ]
-    console.log("People Variable: ", people)
+  { id: '1', name: 'Leigh', age: 35 },
+  { id: '2', name: 'Jenny', age: 30 },
+  { id: '3', name: 'Heather', age: 28 },
+]
+console.log('People Variable: ', people)
 
 let result
 
-
-// Count 
+// Count
 
 result = people.reduce((accumulate, person) => {
-    return accumulate + 1
-
+  return accumulate + 1
 }, 0)
-console.log("Reduce Count Function: ", result)
-
-
+console.log('Reduce Count Function: ', result)
 
 // Sum ages
 
 result = people.reduce((accumulate, person) => {
-    return accumulate + person.age
+  return accumulate + person.age
 }, 0)
-console.log("Reduce Sum of Ages: ", result)
-
-
+console.log('Reduce Sum of Ages: ', result)
 
 // Array of names (map)
 // Using spread syntax
 result = people.reduce((accumulate, person) => {
-
-    return [...accumulate, person.name]
-
+  return [...accumulate, person.name]
 }, [])
-console.log("Array of Names  using spread syntax: ", result)
-
-
+console.log('Array of Names  using spread syntax: ', result)
 
 // Array of names (map)
 // Using push()
 result = people.reduce((accumulate, person) => {
-
-    accumulate.push(person.name)
-    return accumulate
+  accumulate.push(person.name)
+  return accumulate
 }, [])
-console.log("Array if Names using push function", result)
-
+console.log('Array if Names using push function', result)
 
 // Convert to id => person lookup (dictionary)
 
 result = people.reduce((accumulate, person) => {
-
-    return {...accumulate, [person.id]: person }
+  return { ...accumulate, [person.id]: person }
 }, {})
-console.log("Convert to Id person lookup: ", result[2])
-
-
+console.log('Convert to Id person lookup: ', result[2])
 
 // Max age (max value)
 
 result = people.reduce((accumulate, person) => {
-    if (accumulate === null || person.age > accumulate) {
-        return person.age
-    }
-    return accumulate
-
-
+  if (accumulate === null || person.age > accumulate) {
+    return person.age
+  }
+  return accumulate
 }, null)
-console.log("Max Age value:", result)
-
-
+console.log('Max Age value:', result)
 
 // Min age (min value)
 
 result = people.reduce((accumulate, person) => {
-    if (accumulate === null || person.age < accumulate) {
-        return person.age
-    }
-    return accumulate
-
-
+  if (accumulate === null || person.age < accumulate) {
+    return person.age
+  }
+  return accumulate
 }, null)
-console.log("Min age Value: ", result)
-
+console.log('Min age Value: ', result)
 
 // Find by name
 
 result = people.reduce((accumulate, person) => {
+  if (accumulate !== null) {
+    return accumulate
+  }
 
-    if (accumulate !== null) {
-        return accumulate
-    }
-
-    if (person.name === "Leigh") {
-        return person
-    }
-    return null
-
+  if (person.name === 'Leigh') {
+    return person
+  }
+  return null
 }, null)
-console.log("Find by name: ", result)
-
-
+console.log('Find by name: ', result)
 
 // All over 18
 
 result = people.reduce((accumulate, person) => {
-    if (!accumulate) {
-        return false
-
-    }
-    return person.age > 18;
-
+  if (!accumulate) {
+    return false
+  }
+  return person.age > 18
 }, true)
-console.log("Get all people over 18 years", result)
-
-
+console.log('Get all people over 18 years', result)
 
 // Any over 18
 
 result = people.reduce((accumulate, person) => {
-    if (!accumulate) {
-        return true
-
-    }
-    return person.age > 18;
-
+  if (!accumulate) {
+    return true
+  }
+  return person.age > 18
 }, false)
-console.log("Any people over 18", result)
-
+console.log('Any people over 18', result)
 
 // Count occurrences
 
 const orders = [
-    { id: "1", status: "pending" },
-    { id: "2", status: "pending" },
-    { id: "3", status: "cancelled" },
-    { id: "4", status: "shipped" }
+  { id: '1', status: 'pending' },
+  { id: '2', status: 'pending' },
+  { id: '3', status: 'cancelled' },
+  { id: '4', status: 'shipped' },
 ]
 
 result = orders.reduce((accumulate, order) => {
-    return {...accumulate, [order.status]: (accumulate[order.status] || 0) + 1 }
+  return { ...accumulate, [order.status]: (accumulate[order.status] || 0) + 1 }
 }, {})
-console.log("Count how many orders status: ", result)
-
-
+console.log('Count how many orders status: ', result)
 
 // Flatten array
 
-
 const folders = [
-    "index.js", ["flatten.js", "map.js"],
-    ["any.js", ["all.js", "count.js"]],
+  'index.js',
+  ['flatten.js', 'map.js'],
+  ['any.js', ['all.js', 'count.js']],
 ]
 
-
 function flatten(accumulate, item) {
-
-    console.log("accumulate:", accumulate, "item:", item)
-    if (Array.isArray(item)) {
-        return item.reduce(flatten, accumulate)
-    }
-    return [...accumulate, item]
+  console.log('accumulate:', accumulate, 'item:', item)
+  if (Array.isArray(item)) {
+    return item.reduce(flatten, accumulate)
+  }
+  return [...accumulate, item]
 }
 
 result = folders.reduce(flatten, [])
-console.log("Flatten folder", result)
-
+console.log('Flatten folder', result)
 
 // Create reduce function ourselves
 
 function reduce(array, callback, initialValue) {
-    let accumulate = initialValue;
+  let accumulate = initialValue
 
-    for (let i = 0; i < array.length; i++) {
-        accumulate = callback(accumulate, array[i], i, array)
-            console.log("Create a custom reduce function", accumulate)
-
-    }
-    console.log('Create a custom reduce function - Outside the loop: ', accumulate)
-    return accumulate;
+  for (let i = 0; i < array.length; i++) {
+    accumulate = callback(accumulate, array[i], i, array)
+    console.log('Create a custom reduce function', accumulate)
+  }
+  console.log(
+    'Create a custom reduce function - Outside the loop: ',
+    accumulate
+  )
+  return accumulate
 }
 
 result = reduce([1, 2, 3], (accumulate, number) => accumulate + number, 0)
 
-console.log("Reducer result: ", result);
+console.log('Reducer result: ', result)
